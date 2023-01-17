3cd645263d1c1ba53f6b126ced30d4f601920935
カワゴエ <a.kawagow.humankgm@gmail.com>
2023-01-17 16:42:38 +0900

    bool

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "DxLib.h"
+#include "Keyboard.h"
 
 typedef struct {
     int x, y, img, muki, walking_flag;
@@ -48,6 +49,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     if (ChangeWindowMode(TRUE) != DX_CHANGESCREEN_OK || DxLib_Init() == -1) return -1; //ウィンドウ化と初期化処理
 
+
+    LoadDivGraph("画像/キャラクタ10.png", 16, 4, 4, 32, 32, image);//画像を分割してimage配列に保存
     int Handle1 = LoadGraph("画像/Q1.tiff"); // 画像のロード
     int Handle2 = LoadGraph("画像/Q2.tiff");
     int Handle3 = LoadGraph("画像/Q3.tiff");
@@ -66,133 +69,221 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     int KeyHandle1 = LoadGraph("画像/Key1.tiff"); // 画像のロード
     int KeyHandle2 = LoadGraph("画像/Key2.tiff"); // 画像のロード
     int KeyHandle3 = LoadGraph("画像/Key3.tiff"); // 画像のロード
+    int ItemHandle1 = LoadGraph("画像/ItemKey1.tiff"); // 画像のロード
+    int ItemHandle2 = LoadGraph("画像/ItemKey2.tiff"); // 画像のロード
+    int ItemHandle3 = LoadGraph("画像/ItemKey3.tiff"); // 画像のロード
     ch.x = 288;
     ch.y = 416;
     ch.walking_flag = 0;
     ch.muki = 2;
-    bool  keyhandle1 = false;
+    bool  keyhandle1 = false; //アイテム欄の表示
     bool  keyhandle2 = false;
     bool  keyhandle3 = false;
+    bool itemhandle1 = true;
+    bool itemhandle2 = false;
+    bool itemhandle3 = false;
+    bool  keyhandle0 = true;
+
+    bool TextDisplay = false;
+    bool MapDisplay = false;
+
 
     SetDrawScreen(DX_SCREEN_BACK);                                                 //描画先を裏画面に設定
-    LoadDivGraph("画像/キャラクタ10.png", 16, 4, 4, 32, 32, image);//画像を分割してimage配列に保存
+
+
+
+ 
 
 
     while (!ProcessMessage() && !ClearDrawScreen() && !GetHitKeyStateAll(Key) && !Key[KEY_INPUT_ESCAPE]) {
         //↑ﾒｯｾｰｼﾞ処理        ↑画面をｸﾘｱ         ↑ｷｰﾎﾞｰﾄﾞ入力状態取得       ↑ESCが押されると終了
+        Keyboard_Update();
 
-    
+
 
             /*白い壁を描画*/
-            for (i = 0; i < 15; i++)
-                 for (j = 0; j < 20; j++)
-                     if (hantei[i][j] == 1)
-                         DrawBox(j * 32, i * 32, (j + 1) * 32, (i + 1) * 32, GetColor(255, 255, 255), TRUE);
+        for (i = 0; i < 15; i++)
+            for (j = 0; j < 20; j++)
+                if (hantei[i][j] == 1)
+                    DrawBox(j * 32, i * 32, (j + 1) * 32, (i + 1) * 32, GetColor(255, 255, 255), TRUE);
 
-             if (ch.x % 32 == 0 && ch.y % 32 == 0) {         //座標が32で割り切れたら入力可能
-                 ch.walking_flag = 1;         //歩くフラグを立てる。
-                 if (Key[KEY_INPUT_W] == 1)  //上ボタンが押されたら
-                     ch.muki = 0;         //上向きフラグを立てる
-                 else if (Key[KEY_INPUT_A] == 1)  //左ボタンが押されたら
-                     ch.muki = 1;         //左向きフラグを立てる
-                 else if (Key[KEY_INPUT_S] == 1)  //下ボタンが押されたら
-                     ch.muki = 2;         //下向きフラグを立てる
-                 else if (Key[KEY_INPUT_D] == 1)  //右ボタンが押されたら
-                     ch.muki = 3;         //右向きフラグを立てる
-                 else                                    //何のボタンも押されてなかったら
-                     ch.walking_flag = 0; //歩かないフラグを立てる
-                 if (ch.walking_flag == 1)    //もし歩くなら
-                     if (IsAbleToGo(ch.x, ch.y, ch.muki) == 1)//行き先が歩けないなら
-                         ch.walking_flag = 0;                  //歩かないフラグを立てる。
-             }
+        if (ch.x % 32 == 0 && ch.y % 32 == 0) {         //座標が32で割り切れたら入力可能
+            if (!TextDisplay) {
+                ch.walking_flag = 1;         //歩くフラグを立てる。
+                if (Key[KEY_INPUT_W] == 1)  //上ボタンが押されたら
+                    ch.muki = 0;         //上向きフラグを立てる
+                else if (Key[KEY_INPUT_A] == 1)  //左ボタンが押されたら
+                    ch.muki = 1;         //左向きフラグを立てる
+                else if (Key[KEY_INPUT_S] == 1)  //下ボタンが押されたら
+                    ch.muki = 2;         //下向きフラグを立てる
+                else if (Key[KEY_INPUT_D] == 1)  //右ボタンが押されたら
+                    ch.muki = 3;         //右向きフラグを立てる
+                else                                    //何のボタンも押されてなかったら
+                    ch.walking_flag = 0; //歩かないフラグを立てる
+                if (ch.walking_flag == 1)    //もし歩くなら
+                    if (IsAbleToGo(ch.x, ch.y, ch.muki) == 1)//行き先が歩けないなら
+                        ch.walking_flag = 0;                  //歩かないフラグを立てる。
+            }
+        }
 
-             if (ch.walking_flag == 1) {        //歩くフラグが立っていたら
-                 if (ch.muki == 0)        //上向きならch.y座標を減らす
-                     ch.y--;
-                 else if (ch.muki == 1)        //左向きならch.x座標を減らす
-                     ch.x--;
-                 else if (ch.muki == 2)        //下向きならch.y座標を増やす
-                     ch.y++;
-                 else if (ch.muki == 3)        //右向きならch.x座標を増やす
-                     ch.x++;
-             }
-             if (Key[KEY_INPUT_Q] == 1) {//MAP
-                     DrawGraph(0, 0, Map, true);
-                 }
+        if (ch.walking_flag == 1 && TextDisplay == false) {        //歩くフラグが立っていたら
+            if (ch.muki == 0)        //上向きならch.y座標を減らす
+                ch.y--;
+            else if (ch.muki == 1)        //左向きならch.x座標を減らす
+                ch.x--;
+            else if (ch.muki == 2)        //下向きならch.y座標を増やす
+                ch.y++;
+            else if (ch.muki == 3)        //右向きならch.x座標を増やす
+                ch.x++;
+        }
 
+        //テスト用
+        if (MapDisplay) {
+            DrawString(5, 10, "MapDisplay == true", GetColor(0, 0, 0));
+        }
 
-             if (Key[KEY_INPUT_Q] == 1) {//Q1
+        if (Keyboard_Get(KEY_INPUT_M)== 1) {
+           /* if (MapDisplay) {
+                MapDisplay = false;
+            }
+            else {
+                MapDisplay = true;
+            }*/
+            MapDisplay = !MapDisplay;//上の6行で今は言っている値がtrueならfalseにして、falseならtrueにする
+        }
+
+            if(MapDisplay)
+            DrawGraph(0, 0, Map, true);
+ 
+        if (Key[KEY_INPUT_M] == 1) {//Map2
+            if (ch.x == 96 && ch.y == 352 && ch.muki == 2) {
+                Map = LoadGraph("画像/カシオペア座2.tiff");
+            }
+        }
+        if (Key[KEY_INPUT_M] == 1) {//Map3
+            if (ch.x == 64 && ch.y == 64 && ch.muki == 2) {
+                Map = LoadGraph("画像/カシオペア座3.tiff");
+            }
+        }
+        if (Key[KEY_INPUT_M] == 1) {//Map4
+            if (ch.x == 512 && ch.y == 352 && ch.muki == 0) {
+                Map = LoadGraph("画像/カシオペア座4.tiff");
+            }
+        }
+        if (Key[KEY_INPUT_M] == 1) {//Map5
+            if (ch.x == 288 && ch.y == 192 && ch.muki == 3) {
+                Map = LoadGraph("画像/カシオペア座5.tiff");
+            }
+        }
+    
+        //問題文
+        if (TextDisplay) {
+            DrawString(5, 15, "TextDisplay == true", GetColor(0, 0, 0));
+        }
+            if (Keyboard_Get(KEY_INPUT_Q) == 1) {
+                /* if (MapDisplay) {
+                     MapDisplay = false;
+                }
+                else {
+                     MapDisplay = true;
+                }*/
+                TextDisplay = !TextDisplay;//上の6行で今は言っている値がtrueならfalseにして、falseならtrueにする
+            }
+            
+             if (TextDisplay) {//Quest1
                  if (ch.x == 32 && ch.y == 288 && ch.muki == 0) {
                      DrawGraph(0, 0, Handle1, true);
                  }
              }
          
-             if (Key[KEY_INPUT_Q] == 1) {//Q3
-                 if (ch.x == 352 && ch.y ==320 && ch.muki == 1) {
+             if (TextDisplay) {//Quest3
+                 if (ch.x == 352 && ch.y ==288 && ch.muki == 1) {
                      DrawGraph(0, 0, Handle3, true);
                  }
              }
-             if (Key[KEY_INPUT_Q] == 1) {//Q4
+             if (TextDisplay) {//Quest4
                  if (ch.x == 352 && ch.y == 416 && ch.muki == 1) {
                      DrawGraph(0, 0, Handle4, true);
                  }
              }
-             if (Key[KEY_INPUT_Q] == 1) {//i1
-                 if (ch.x == 96 && ch.y == 352 && ch.muki == 2) {
-                     DrawGraph(0, 0, Handle1, true);
-                     Map = LoadGraph("画像/カシオペア座2.tiff");
-                     keyhandle1 = true;
-                 }
-             }
-             if (Key[KEY_INPUT_Q] == 1) {//i2
-                 if (ch.x == 64 && ch.y == 64 && ch.muki == 2) {
-                     DrawGraph(0, 0, Handle2, true);
-                     Map = LoadGraph("画像/カシオペア座3.tiff");
-                     keyhandle2 = true;
-                 }
-             }
-             if (Key[KEY_INPUT_Q] == 1) {//i4
-                 if (ch.x == 512 && ch.y == 352 && ch.muki == 0) {
-                     DrawGraph(0, 0, Handle1, true);
-                     Map = LoadGraph("画像/カシオペア座4.tiff");
-                     keyhandle3 = true;
-                 }
 
-             }
-             if (Key[KEY_INPUT_Q] == 1) {//☆1
+
+             //ヒント
+             if (TextDisplay) {//☆1
                  if (ch.x == 288 && ch.y == 384 && ch.muki == 1) {
                      DrawGraph(0, 0, Question1, true);
                  }
              }
-             if (Key[KEY_INPUT_Q] == 1) {//☆2
+             if (TextDisplay) {//☆2
                  if (ch.x == 288 && ch.y == 320 && ch.muki == 3) {
                      DrawGraph(0, 0, Question2, true);
                  }
              }
         
-             if (Key[KEY_INPUT_Q] == 1) {//☆3
+             if (TextDisplay) {//☆3
                  if (ch.x == 288 && ch.y == 256 && ch.muki == 1) {
                      DrawGraph(0, 0, Handle2, true);
                  }
              }
-             if (Key[KEY_INPUT_Q] == 1) {//☆4
+             if (TextDisplay) {//☆4
                  if (ch.x == 288 && ch.y == 192 && ch.muki == 3) {
-                     Map = LoadGraph("画像/カシオペア座5.tiff");
+                     DrawBox(150, 350, 450, 450, (255, 255, 255), true);
+                     DrawFormatString(170, 370, (0, 0, 0), "最後の地図を見つけた!!", i);
+                     DrawFormatString(170, 400, (0, 0, 0), "地図に変化が...", i);
+
                  }
              }
-             if (Key[KEY_INPUT_Q] == 1) {//☆5
+             if (TextDisplay) {//☆5
                  if (ch.x == 288 && ch.y == 96 && ch.muki == 1) {
                      DrawGraph(0, 0, Question3, true);
                  }
              }
-             ch.img = image[(ch.x % 32 + ch.y % 32) / 8 + ch.muki * 4];            //画像をセット
-             DrawFormatString(0, 0, (255, 255, 255), " % d", ch.x);
-             DrawFormatString(0, 20, (255, 255, 255), " % d", ch.y);
-             for (int i = 0; i < 15; i++) {
-                 if (i++){
-                     DrawBox(30 + i*30, 0, 90 + i*30, 60, GetColor(0, 0, 0), false);
+
+             //アイテム
+
+             if (TextDisplay) {//item1
+                 if (ch.x == 96 && ch.y == 352 && ch.muki == 2) {
+                     if (keyhandle0 == true) {
+                         DrawGraph(50, 350, ItemHandle1, true);
+                         DrawBox(150, 350, 450, 450, (255, 255, 255), true);
+                         DrawFormatString(170, 355, (0, 0, 0), "アイテム入手!!", i);
+                         DrawFormatString(170, 390, (0, 0, 0), "左上の部屋のヒントを見つけた!", i);
+                         DrawFormatString(170, 410, (0, 0, 0), "おや、地図に変化が...", i);
+
+                         keyhandle1 = true;
+                         itemhandle1 = false;
+                         keyhandle0 = false;
+                     }
+                     else if (keyhandle0 == false) {
+                         DrawBox(150, 350, 450, 450, (255, 255, 255), true);
+                         DrawFormatString(170, 355, (0, 0, 0), "何もないようだ", i);
+                     }
                  }
-               
+   
+             }
+             
+             if (TextDisplay) {//item2
+                 if (ch.x == 64 && ch.y == 64 && ch.muki == 2) {
+                     DrawGraph(50, 350, ItemHandle2, true);
+                     DrawBox(150, 350, 450, 450, (255, 255, 255), true);
+                     DrawFormatString(170, 355, (0, 0, 0), "アイテム入手!!", i);
+                     DrawFormatString(170, 390, (0, 0, 0), "右下の部屋のヒントを見つけた!", i);
+                     DrawFormatString(170, 420, (0, 0, 0), "おや、地図に変化が...", i);
+
+                     keyhandle2 = true;
+                 }
+             }
+             if (TextDisplay) {//item4
+                 if (ch.x == 512 && ch.y == 352 && ch.muki == 0) {
+                     DrawGraph(50, 350, ItemHandle3, true);
+                     DrawBox(150, 350, 450, 450, (255, 255, 255), true);
+                     DrawFormatString(170, 355, (0, 0, 0), "アイテム入手!!", i);
+                     DrawFormatString(170, 390, (0, 0, 0), "右上の部屋のヒントを見つけた!", i);
+                     DrawFormatString(170, 420, (0, 0, 0), "おや、地図に変化が...", i);
+
+                     keyhandle3 = true;
+                 }
+
              }
              if (keyhandle1 == 1) {
                  DrawGraph(125, 5, KeyHandle1, true);//アイテム鍵1
@@ -203,9 +294,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
              if (keyhandle3 == 1) {
                  DrawGraph(245, 5, KeyHandle3, true);//アイテム鍵1
              }
-             DrawGraph(ch.x, ch.y, ch.img, TRUE);//画像を描画
+             ch.img = image[(ch.x % 32 + ch.y % 32) / 8 + ch.muki * 4];            //画像をセット
+             DrawFormatString(0, 0, (255, 255, 255), " % d", ch.x);
+             DrawFormatString(0, 20, (255, 255, 255), " % d", ch.y);
+             for (int i = 0; i < 9; i++) {
+                 if (i++) {
+                     DrawBox(30 + i * 30, 0, 90 + i * 30, 60, GetColor(0, 0, 0), false);
+                 }
+
+             }
+             if (!TextDisplay) {
+                 DrawGraph(ch.x, ch.y, ch.img, TRUE);//画像を描画
+             }
              DrawRotaGraph(400, 200, 1.0, 0, Image[64], TRUE); //画像の描画
              DrawRotaGraph(400, 300, 1.0, 0, Image[98], TRUE); //画像の描画
+
              //DrawGraph(125, 5, KeyHandle1, true);//アイテム鍵1
              //DrawGraph(185, 5, KeyHandle2, true);//アイテム鍵2
              //DrawGraph(245, 5, KeyHandle3, true);//アイテム鍵3
